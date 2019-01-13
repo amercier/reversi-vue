4d5bc75d1b593c0f15d5b47e35f63869cc017f1c
Alex Mercier <pro.alexandre.mercier@gmail.com>
2019-01-13 08:35:27 +0100

    Added Header and styles to App component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,51 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <ul>
-        <li><router-link to="/">Home</router-link></li>
-        <li><router-link to="/about">About</router-link></li>
-      </ul>
-    </div>
-    <router-view />
+  <div class="app">
+    <Header class="app-header" />
+    <router-view class="app-content" />
   </div>
 </template>
+
+<script>
+import Header from '@/components/Header.vue';
+
+/**
+ * Application root component.
+ */
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style lang="scss">
+body {
+  background: radial-gradient(
+      circle at top right,
+      rgba(220, 180, 50, 0.15) 0,
+      rgba(220, 180, 50, 0) 100%
+    ),
+    url(./assets/wooden-planks.jpg) repeat top left;
+  color: white;
+  font-family: 'Roboto Slab', serif;
+  font-size: calc(8px + 1.2vmin);
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+.app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  overflow-y: hidden;
+
+  &-content {
+    flex-grow: 1;
+    height: 100vh;
+    overflow-y: auto;
+  }
+}
+</style>
