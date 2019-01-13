152cc9089fdd406cb15bbd5f85b4ec27ca2ed1ad
Alex Mercier <pro.alexandre.mercier@gmail.com>
2019-01-13 18:32:45 +0100

    Updated Board component with new store getters

--- tests/unit/components/Board.spec.js
+++ tests/unit/components/Board.spec.js
@@ -2,14 +2,22 @@ import { shallowMount } from '@vue/test-utils';
 import Board from '@/components/Board.vue';
 
 describe('Board.vue', () => {
+  const propsData = {
+    cells: [
+      { row: 0, col: 0, isStone: true, isMove: false, color: 'white' },
+      { row: 0, col: 1, isStone: true, isMove: false, color: 'black' },
+      { row: 1, col: 0, isStone: false, isMove: true, color: 'black' },
+      { row: 1, col: 1, isStone: false, isMove: false, color: 'blank' },
+    ],
+    onMove: () => {},
+  };
+
   it('renders without crashing', () => {
-    shallowMount(Board);
+    shallowMount(Board, { propsData });
   });
 
   it('renders the board', () => {
-    const board = shallowMount(Board, {
-      propsData: {},
-    });
+    const board = shallowMount(Board, { propsData });
     expect(board.html()).toMatchSnapshot();
   });
 });
