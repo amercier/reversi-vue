88397c52dd14f59505a1fe2677cba25843214517
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-22 17:33:29 +0000

    Deploy amercier/reversi-vue to github.com/amercier/reversi-vue.git:deploy

---
+++ precache-manifest.db92cc47896efeed540a152c543d92c2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "74dda019286ac779aadce4ee69a4e865",
+    "url": "/img/wooden-planks.74dda019.jpg"
+  },
+  {
+    "revision": "ac53cf6f7e32a32341e2faab85100b92",
+    "url": "/site.webmanifest"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "5a9367b6229464d50fec",
+    "url": "/js/chunk-vendors.bf563af5.js"
+  },
+  {
+    "revision": "ad428705a11c49e4db6b",
+    "url": "/js/app.3bd711f0.js"
+  },
+  {
+    "revision": "7e1510a21739501603721a67371185d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "888f0e455e1afd0b86caf444d7914d3c",
+    "url": "/img/wooden-board.888f0e45.jpg"
+  },
+  {
+    "revision": "1e5165b95e7a73afaa83",
+    "url": "/css/critical.3e6c2a9e.css"
+  },
+  {
+    "revision": "ad428705a11c49e4db6b",
+    "url": "/css/app.836ad14e.css"
+  },
+  {
+    "revision": "3b8769c657d761a3ad70a7935c28c97b",
+    "url": "/browserconfig.xml"
+  },
+  {
+    "revision": "70b2a6ffaed18e57256265f6e7a75bab",
+    "url": "/_redirects"
+  }
+];
