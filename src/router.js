69185f4d2846da9181ad6bd43712e0f0a47b550f
Alex Mercier <pro.alexandre.mercier@gmail.com>
2019-01-22 18:16:44 +0100

    Disabled route level code-splitting for /about

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Game from './views/Game.vue';
+import About from './views/About.vue';
 import NotFound from './views/NotFound.vue';
 
 Vue.use(Router);
@@ -17,11 +18,7 @@ export default new Router({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ './views/About.vue'),
+      component: About,
     },
     {
       path: '/404',
